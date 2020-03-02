f762d10b0b77ce9eecafa4672028c35ee2766b1d
Capn Flint <capnflint@gmail.com>
2020-03-02 01:20:19 +0000

    bug fix

--- src/modules/citizen/model.js
+++ src/modules/citizen/model.js
@@ -4,7 +4,7 @@ const { executeSQL } = require('../mariadb')
 const { getUser } = require('../user/model')
 const { fetchCitizen } = require('../../helpers/rsi')
 const { getID } = require('../../helpers/db')
-const { syncCitizen } = require('../user')
+const { syncCitizen } = require('../user/model')
 
 /*
 *   GET /citizen/<handle>
