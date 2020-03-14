b58911cebbd7ea934658cc54bbc7f648b0cc627b
Capn Flint <capnflinttv@gmail.com>
2020-03-14 15:00:32 -0700

    continue merged news feed

--- src/modules/news/model.js
+++ src/modules/news/model.js
@@ -1,5 +1,8 @@
 const axios = require("axios")
 const cheerio = require('cheerio')
+const { sub, isBefore } = require('date-fns')
+
+const { getFeed } = require('./rss')
 
 async function fetchNews(data) {
     try {
@@ -39,11 +42,56 @@ async function fetchNews(data) {
 }
 
 function computeDate(posted) {
-    
+    if(posted.startsWith('about')) {
+        posted = posted.substring(6)
+    }
+    const [count, unit] = posted.split(' ')
+    let date = null
+    if (unit.startsWith('second')) {
+        date = sub(new Date(), {seconds: count})
+    } else if (unit.startsWith('minute')) {
+        date = sub(new Date(), {minutes: count})
+    } else if (unit.startsWith('hour')) {
+        date = sub(new Date(), {hours: count})
+    } else if (unit.startsWith('day')) {
+        date = sub(new Date(), {days: count})
+    } else if (unit.startsWith('week')) {
+        date = sub(new Date(), {weeks: count})
+    } else if (unit.startsWith('month')) {
+        date = sub(new Date(), {months: count})
+    } else if (unit.startsWith('year')) {
+        date = sub(new Date(), {years: count})
+    }
+    return date.toUTCString()
+}
+
+function mergeNews(first, second) {
+    let result = []
+    while (first.length + second.length > 0) {
+        console.log(first[0], second[0])
+        if(first.length === 0) {
+            console.log('a')
+            result = result.concat(second)
+            second = []
+        } else if (second.length === 0) {
+            console.log('b')
+            result = result.concat(first)
+            first = []
+        } else if (isBefore(new Date(first[0].posted_date), new Date(second[0].posted_date))) {
+            console.log('c')
+            result.push(second.shift())
+        } else {
+            console.log('d')
+            result.push(first.shift())
+        }
+    }
+    return result
 }
 
 async function getNews(data) {
-    return await fetchNews(data)
+    const rsiNews = await fetchNews(data)
+    const impgeo = await getFeed()
+    return mergeNews(rsiNews, impgeo)
 }
 
 module.exports = {
--- src/modules/news/rss.js
+++ src/modules/news/rss.js
@@ -1,19 +1,59 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
+const { format, formatDistance, subDays } = require('date-fns')
 
 async function loadRSS(link) {
-    await axios({
+    return await axios({
+        url: link,
+        method: 'GET'
+    }).then(async (res) => {
+        let items = []
+        const $ = cheerio.load(res.data, { xmlMode: true })
+        const source_img = $('image').find('url').text()
+        $('item').each((i, el) => {
+            const item = {}
+            item.source = 'impgeo'
+            item.source_img = source_img
+            item.id = $(el).find('guid').text().split('?p=')[1]
+            item.title = $(el).find('title').text()
+            item.image = 'https://www.imperialgeographic-official.org/wp-content/uploads/2019/11/Bivaly_ScreenShot10.jpg'
+            item.link = $(el).find('link').text()
+            item.posted_date = $(el).find('pubDate').text()
+            item.posted = formatDistance(new Date(item.posted_date), new Date()) + " ago"
+            
+            items.push(item)
+        })
+        //items = await addImages(items)
+
+        return items
+    }).catch((err) => {
+        console.error(err)
+    })
+}
+
+async function addImages(items) {
+    for(i in items) {
+        console.log(i)
+        items[i].image = await getArticleImage(items[i].link)
+    }
+    return items
+}
+
+async function getArticleImage(link) {
+    return axios({
         url: link,
         method: 'GET'
     }).then((res) => {
         const $ = cheerio.load(res.data)
-        console.log($('image').find('url').text())
-        return $('image')
+        const image = $('article').find('img.wp-post-image').attr('src')
+        return image
+    }).catch((err) => {
+        console.error(err)
     })
 }
 
 async function getFeed() {
-    return loadRSS('https://www.imperialgeographic-official.org/feed/')
+    return await loadRSS('https://www.imperialgeographic-official.org/feed/')
 }
 
 module.exports = {
