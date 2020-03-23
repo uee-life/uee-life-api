d73cc5d26bff0b9143f9e26850f2d37ace8992ec
Capn Flint <capnflinttv@gmail.com>
2020-03-23 14:24:10 -0700

    Update model.js

--- src/modules/citizen/model.js
+++ src/modules/citizen/model.js
@@ -218,7 +218,7 @@ async function searchCitizen(search) {
                 portrait: avatar,
                 org: org
             }
-            data.citizens.push(cit)
+            data.push(cit)
         }
         return data
     } else {
