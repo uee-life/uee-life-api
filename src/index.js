3a47b457a0e523dfae76ecb78f9ba1d77452242e
Capn Flint <capnflinttv@gmail.com>
2022-12-05 19:56:29 -0800

    package updates and deprecation fix.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // importing the dependencies
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
@@ -9,8 +8,8 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(helmet());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
 app.use(cors());
 app.use(morgan('combined'));
 
